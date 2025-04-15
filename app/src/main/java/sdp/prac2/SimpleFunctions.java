c704faca9a30c991391678159bb24b75802e7db3
hlumax <143754308+hlumax@users.noreply.github.com>
2025-04-15 12:18:37 +0200

    Update SimpleFunctions.java

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,17 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public static int sumElements(int []a, int []b) {
+        int sum = 0;
+        
+        for (int index : b) {
+            // Check if index is within bounds of 'a'
+            if (index >= 0 && index < a.length) {
+                sum += a[index];
+            }
+            // Ignore invalid indexes
+        }
+        
+        return sum;
+    }
 }
